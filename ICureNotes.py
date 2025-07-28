17c89554574c87d4cd6e70763452deb41ef9e0e0
Corporal Rat <86330761+SergeantWiley@users.noreply.github.com>
2025-07-27 18:14:41 -0700

    Add files via upload

--- ICureNotes.py
+++ ICureNotes.py
@@ -1,5 +1,19 @@
 import numpy as np
 import pandas as pd
+import faiss
+#Make Random vectors
+data = np.random.random((1000,128)).astype('float32')
+#Make random vectors with int from 1 to 100
+data = np.random.randint(1,101,size=(1000,128)).astype('float32')
 
-data = np.random.random((1000,128))
-print(data)
+#df = pd.DataFrame(data)
+#df.to_csv("Vectors.csv",index=False)
+
+vector = np.random.randint(1,101,size=(1,128))
+print(vector)
+data = pd.read_csv("Vectors.csv",index_col=False).to_numpy()
+
+index = faiss.IndexFlatL2(128)
+index.add(data)
+distances,indices = index.search(vector,k=1)
+print(data[indices[0]])
